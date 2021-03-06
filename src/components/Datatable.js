176f099899bce1bb95a748c9380c9259469f201a
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-06 04:17:31 +0300

    update veri almak için axios yazıldı

--- src/components/Datatable.js
+++ src/components/Datatable.js
@@ -3,6 +3,18 @@ import axios from "axios"
 
 
 function Datatable() {
+    const [AllData, setAllData] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://restcountries.eu/rest/v2/all`)
+            .then(function ({ data }) {
+                console.log(data)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }, [])
+
     return (
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae voluptatem distinctio quos laboriosam deleniti nostrum odit a asperiores. Aperiam ad nisi quos iusto quod? Vero aperiam cumque cum ratione quis?</p>
     )
