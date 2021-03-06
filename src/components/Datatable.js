a7efdf094c58b716722b0119928a1fb7d5aec296
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-06 19:42:49 +0300

    update page.css import edildi

--- src/components/Datatable.js
+++ src/components/Datatable.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 import { Table, Row, Col } from 'react-bootstrap';
 import { TableHead, TableRow, Search } from '../components map/Components'
+/* CSS */
+import '../css/page.css'
 
 
 function Datatable() {
@@ -59,8 +61,8 @@ function Datatable() {
             <Row>
                 <Col>
                     <section>
-                        <div className="overflow-auto">
-                            <Table striped bordered hover className="table">
+                        <div className="dataTableSection">
+                            <Table striped bordered hover>
                                 <TableHead name={"Name"} capital={"Capital"} flag={"Flag"} />
                                 <tbody>
                                     {tableData}
