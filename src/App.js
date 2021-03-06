2ed2162c83f33ad25bf6dd71e98e1b3bde800405
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-06 04:06:24 +0300

    update Router ve bootstrap import edildi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
+import { Button, Container, Row } from 'react-bootstrap';
+
+/* CSS */
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      test
+
     </div>
   );
 }
