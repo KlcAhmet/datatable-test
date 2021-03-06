35574082da89a6ef16038d38ccc9ed51c81ffc9c
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-06 04:13:55 +0300

    add Datateble.js

--- src/components/Datatable.js
+++ src/components/Datatable.js
@@ -1,2 +1,11 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
+
+
+function Datatable() {
+    return (
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae voluptatem distinctio quos laboriosam deleniti nostrum odit a asperiores. Aperiam ad nisi quos iusto quod? Vero aperiam cumque cum ratione quis?</p>
+    )
+}
+
+export default Datatable
