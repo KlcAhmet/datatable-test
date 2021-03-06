1dcea93c0d81a5e854d255c51b9336210f9a5b06
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-06 04:06:55 +0300

    update Router test içerik yazıldı

--- src/App.js
+++ src/App.js
@@ -12,7 +12,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Container>
+            <Row>
+              <Route path="/">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta eaque architecto at quis accusantium ut, a quod ullam delectus dolore, cum error dignissimos odio rerum, eos aut iste veritatis? Quia!
+                <Button>test</Button>
+              </Route>
+            </Row>
+          </Container>
+        </Switch>
+      </Router >
     </div>
   );
 }
