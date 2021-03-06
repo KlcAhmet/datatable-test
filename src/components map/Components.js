4d9fc447dcc26aa8477e9f52ed88403433768484
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-06 06:32:06 +0300

    update Search componenti eklendi

--- src/components map/Components.js
+++ src/components map/Components.js
@@ -1,3 +1,4 @@
 export { default as TableHead } from "../components/TableHead"
 export { default as TableRow } from "../components/TableRow"
 export { default as Flag } from "../components/Flag"
+export { default as Search } from "../components/Search"
